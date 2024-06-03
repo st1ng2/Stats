3c03c4023f4a425b90a5c282329ae52449a2db64
Bahodir <st1ng2@protonmail.ch>
2024-06-03 19:31:21 +0500

    update: Uzbek translations

---
+++ i18n/uz/stats.php
@@ -0,0 +1,36 @@
+<?php
+
+return [
+    'title' => 'Oʻyinchilar statistikasi',
+    'description' => 'Ushbu sahifada siz global oʻyinchilar statistikasini koʻrishingiz mumkin',
+    'module_description' => 'Ushbu modul serverdagi oʻyinchi statistikasini koʻrsatadi',
+    'server_not_found' => 'Server topilmadi',
+    'params_empty' => 'Baʻzi parametrlar boʻsh',
+    'admin' => [
+        'title' => 'Statistika moduli',
+    ],
+    'profile' => [
+        'no_info' => 'Maʻlumot yoʻq 😢',
+        'user' => 'Oʻyinchi statistikasi - :name',
+        'head' => 'Statistika',
+        'value' => 'Ballar',
+        'kills' => 'Oʻldirishlar',
+        'deaths' => 'Oʻlimlar',
+        'shoots' => 'Otishmalar',
+        'experience' => 'Tajriba',
+        'score' => 'Ballar',
+        'hits' => 'Xitlar',
+        'headshots' => 'Boshga',
+        'assists' => 'Yordam',
+        'round_win' => 'Raund gʻalabalari',
+        'round_lose' => 'Raund magʻlubiyatlari',
+    ],
+    'name' => 'Ism',
+    'rank' => 'Daraja',
+    'score' => 'Ballar',
+    'kills' => 'Oʻldirishlar',
+    'deaths' => 'Oʻlimlar',
+    'last_active' => 'Oxirgi aktivlik',
+    'expirience' => 'Tajriba',
+    'choose_server' => "Serverni tanlang"
+];
